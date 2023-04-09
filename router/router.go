73ff3c40c18b85abc03f309c938d7cc126db9eee
fu.lin <river@vvl.me>
2023-04-09 19:41:23 +0800

    router: only chatgpt-web api
    Signed-off-by: fu.lin <river@vvl.me>

--- router/router.go
+++ router/router.go
@@ -2,7 +2,7 @@ package router
 
 import (
 	apiv0 "cheatppt/api/v0"
-	"cheatppt/api/v1"
+	//"cheatppt/api/v1"
 
 	"github.com/gin-gonic/gin"
 )
@@ -26,28 +26,29 @@ const (
 )
 
 func Initialize(router *gin.Engine) {
-	router.LoadHTMLGlob("templates/*")
+	//router.LoadHTMLGlob("templates/*")
 
 	/* v2 just for no-authorization request */
 	rv0 := router.Group("/api/v0")
 	{
-		rv0.POST(chatgptWebVerify, apiv0.ChatgptWebVerify)
-		rv0.POST(chatgptWebSession, apiv0.ChatgptWebSession)
+		chatgptweb := rv0.Group("/api")
+		chatgptweb.POST(chatgptWebVerify, apiv0.ChatgptWebVerify)
+		chatgptweb.POST(chatgptWebSession, apiv0.ChatgptWebSession)
 
-		rv0.Use(apiv0.ChatgptWebAuth)
-		rv0.POST(chatgptWebChatProcess, apiv0.ChatgptWebChatProcess)
-		rv0.POST(chatgptWebConfig, apiv0.ChatgptWebConfig)
+		chatgptweb.Use(apiv0.ChatgptWebAuth)
+		chatgptweb.POST(chatgptWebChatProcess, apiv0.ChatgptWebChatProcess)
+		chatgptweb.POST(chatgptWebConfig, apiv0.ChatgptWebConfig)
 	}
 
-	apiv1 := router.Group("/api/v1")
-	{
-		apiv1.POST(register, api.UserRegister)
-		apiv1.POST(login, api.UserLogin)
-		apiv1.GET(authorized, api.UserAuthorized)
-		apiv1.POST(emailVerfiy, api.EmailVerfiy)
-		apiv1.POST(reset, api.UserPasswordReset)
-		apiv1.GET(logout, api.UserLogout)
-		apiv1.POST(chatProcess, api.ChatProcess)
-		apiv1.GET(listModels, api.ListModels)
-	}
+	//apiv1 := router.Group("/api/v1")
+	//{
+	//	apiv1.POST(register, api.UserRegister)
+	//	apiv1.POST(login, api.UserLogin)
+	//	apiv1.GET(authorized, api.UserAuthorized)
+	//	apiv1.POST(emailVerfiy, api.EmailVerfiy)
+	//	apiv1.POST(reset, api.UserPasswordReset)
+	//	apiv1.GET(logout, api.UserLogout)
+	//	apiv1.POST(chatProcess, api.ChatProcess)
+	//	apiv1.GET(listModels, api.ListModels)
+	//}
 }
